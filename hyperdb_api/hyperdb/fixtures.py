1dc3ad0e44a18cc5da95fe3b4312858378baad5e
Stephen Hartzell <hartzell.stephen@gmail.com>
2023-04-04 20:28:07 -0400

    Added fixtures code

--- hyperdb_api/hyperdb/fixtures.py
+++ hyperdb_api/hyperdb/fixtures.py
@@ -11,7 +11,21 @@ FIXTURES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '
 
 def generate_fixtures(
         location: Union[str, None] = None,
-        num_countries: int = 5
+        num_systems: int = 4, 
+        num_countries = 5, 
+        num_country_system_associations=10,
+        num_contributors=5,
+        num_geometries=15,
+        num_meshes=30,
+        num_tools=10,
+        num_cbaero_settings=15,
+        num_cart3d_settings=15,
+        num_tool_settings=10,
+        num_configured_tools=10,
+        num_tool_mesh_associations=20,
+        num_tool_geometry_associations=20,
+        num_aero_results=200,
+        num_comments=20
     ):
     """Randomly generate fixtures data
     
@@ -29,8 +43,39 @@ def generate_fixtures(
     else:
         fixtures_file = location
 
+    systems = [models.System.generate_random() for _ in range(num_systems)]
+    countries = [models.Country.generate_random(i) for i in range(num_countries)]
+    country_system_associations = [models.CountrySystemAssociation.generate_random(len(countries), len(systems)) for _ in range(num_country_system_associations)]
+    contributors = [models.Contributor.generate_random() for _ in range(num_contributors)]
+    geometries = [models.Geometry.generate_random(num_contributors, num_systems) for _ in range(num_geometries)]
+    meshes = [models.Mesh.generate_random(num_contributors, num_geometries) for _ in range(num_meshes)]
+    tools = [models.Tool.generate_random() for _ in range(num_tools)]
+    cbaero_settings = [models.CBAeroSetting.generate_random() for _ in range(num_cbaero_settings)]
+    cart3d_settings = [models.Cart3DSetting.generate_random() for _ in range(num_cart3d_settings)]
+    tool_settings = [models.ToolSetting.generate_random(min([num_cart3d_settings, num_cbaero_settings])) for _ in range(num_tool_settings)]
+    configured_tools = [models.ConfiguredTool.generate_random(num_tools, num_tool_settings) for _ in range(num_configured_tools)]
+    tool_mesh_associations = [models.ToolMeshAssociation.generate_random(num_contributors, num_meshes, num_tools, num_configured_tools) for _ in range(num_tool_mesh_associations)]
+    tool_geometry_associations = [models.ToolGeometryAssociation.generate_random(num_contributors, num_geometries, num_tools, num_configured_tools) for _ in range(num_tool_geometry_associations)]
+    aero_results = [models.AeroResult.generate_random(num_configured_tools, num_meshes) for _ in range(num_aero_results)]
+    comments = [models.Comment.generate_random(num_contributors, num_systems, num_geometries, num_meshes, num_tool_mesh_associations, num_configured_tools) for _ in range(num_comments)]
+    
     fixtures = {
-        "countries": [models.Country.generate_random().to_dict() for _ in range(num_countries)]
+        "systems": [system.to_dict() for system in systems],
+        "countries": [country.to_dict() for country in countries],
+        "country_system_associations": [
+            country_system_association.to_dict() for country_system_association in country_system_associations],
+        "contributors": [contributor.to_dict() for contributor in contributors],
+        "geometries": [geometry.to_dict() for geometry in geometries],
+        "meshes": [mesh.to_dict() for mesh in meshes],
+        "tools": [tool.to_dict() for tool in tools],
+        "cbaero_settings": [cbaero_setting.to_dict() for cbaero_setting in cbaero_settings],
+        "cart3d_settings": [cart3d_setting.to_dict() for cart3d_setting in cart3d_settings],
+        "tool_settings": [tool_setting.to_dict() for tool_setting in tool_settings],
+        "configured_tools": [configured_tool.to_dict() for configured_tool in configured_tools],
+        "tool_mesh_associations": [tool_mesh_association.to_dict() for tool_mesh_association in tool_mesh_associations],
+        "tool_geometry_associations": [tool_geometry_association.to_dict() for tool_geometry_association in tool_geometry_associations],
+        "aero_results": [aero_result.to_dict() for aero_result in aero_results],
+        "comments": [comment.to_dict() for comment in comments],
     }
     with open(fixtures_file, 'w') as fid:
         json.dump(fixtures, fid)
@@ -59,7 +104,7 @@ if __name__ == "__main__":
     if not os.path.exists(FIXTURES_DIR):
         os.mkdir(FIXTURES_DIR)
     fixtures_file = os.path.join(FIXTURES_DIR, 'fixtures.json')
-    if not os.path.isfile(fixtures_file):
-        generate_fixtures()
+    #if not os.path.isfile(fixtures_file):
+    generate_fixtures()
     
     create_fixtures(SessionLocal(), fixtures_file)
--- hyperdb_api/hyperdb/models.py
+++ hyperdb_api/hyperdb/models.py
@@ -1,7 +1,10 @@
+import lorem
 import pycountry
 from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, Float
 from sqlalchemy.orm import Mapped, mapped_column
 from hyperdb.database import Base
+from names import get_first_name, get_full_name
+import random
 
 
 class ToDictMixin(object):
@@ -10,30 +13,36 @@ class ToDictMixin(object):
         return {k: v for k, v in self.__dict__.items() if k[0] != "_"}
 
 
-class System(Base):
+class System(Base, ToDictMixin):
     __tablename__ = "systems"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name = Column(String(128))
     classification = Column(String(128))
 
+    @classmethod
+    def generate_random(cls):
+        name = get_first_name()
+        classification = "UNCLASSIFIED"
+
+        return cls(name=name, classification=classification)
+
 
 class Country(Base, ToDictMixin):
     __tablename__ = "countries"
-    __generated_instances = 0
 
     id: Mapped[int] = mapped_column(primary_key=True)
     alpha_three_code = Column(String(3), unique=True)
     icon = Column(String(128), unique=True)
 
     @classmethod
-    def generate_random(cls):
-        country = list(pycountry.countries)[cls.__generated_instances % len(pycountry.countries)]
+    def generate_random(cls, instance):
+        country = list(pycountry.countries)[instance % len(pycountry.countries)]
 
         return cls(alpha_three_code=country.alpha_3)
 
 
-class CountrySystemAssociations(Base):
+class CountrySystemAssociation(Base, ToDictMixin):
     """
     Based on https://docs.sqlalchemy.org/en/20/core/metadata.html#sqlalchemy.schema.Column.index
     """
@@ -43,8 +52,15 @@ class CountrySystemAssociations(Base):
     system_fk: Mapped[int] = mapped_column(ForeignKey("systems.id"))
     country_fk: Mapped[int] = mapped_column(ForeignKey("countries.id"))
 
+    @classmethod
+    def generate_random(cls, num_countries: int, num_systems: int):
+        country_fk = random.randint(0, num_countries-1)
+        system_fk = random.randint(0, num_systems-1)
 
-class Contributor(Base):
+        return cls(country_fk=country_fk, system_fk=system_fk)
+
+
+class Contributor(Base, ToDictMixin):
     """
     """
     __tablename__ = "contributors"
@@ -53,8 +69,15 @@ class Contributor(Base):
     name = Column(String(64))
     email = Column(String(64), unique=True)
 
+    @classmethod
+    def generate_random(cls):
+        name = get_full_name()
+        email = name.replace(" ", ".") + "@hyperdb.com"
 
-class Geometry(Base):
+        return cls(name=name, email=email)
+
+
+class Geometry(Base, ToDictMixin):
     """
     """
     __tablename__ = "geometries"
@@ -65,8 +88,16 @@ class Geometry(Base):
     system_fk: Mapped[int] = mapped_column(ForeignKey("systems.id"))
     classification = Column(String(128))
 
+    @classmethod
+    def generate_random(cls, num_contributors: int, num_systems: int):
+        file = get_first_name() + ".IGS"
+        contributor_fk = random.randint(0, num_contributors-1)
+        system_fk = random.randint(0, num_systems-1)
 
-class Mesh(Base):
+        return cls(file=file, contributor_fk=contributor_fk, system_fk=system_fk, classification="UNCLASSIFIED")
+
+
+class Mesh(Base, ToDictMixin):
     """
     """
     __tablename__ = "meshes"
@@ -76,8 +107,16 @@ class Mesh(Base):
     contributor_fk: Mapped[int] = mapped_column(ForeignKey("contributors.id"))
     geometry_fk: Mapped[int] = mapped_column(ForeignKey("geometries.id"))
 
+    @classmethod
+    def generate_random(cls, num_contributors: int, num_geometries: int):
+        file = get_first_name() + ".MSH"
+        contributor_fk = random.randint(0, num_contributors-1)
+        geometry_fk = random.randint(0, num_geometries-1)
 
-class Tool(Base):
+        return cls(file=file, contributor_fk=contributor_fk, geometry_fk=geometry_fk)
+
+
+class Tool(Base, ToDictMixin):
     """
     """
     __tablename__ = "tools"
@@ -87,8 +126,15 @@ class Tool(Base):
     name = Column(String(64))
     version = Column(String(32))
 
+    @classmethod
+    def generate_random(cls):
+        name = get_first_name()
+        version = "{}.{}.{}".format(random.randint(0, 9), random.randint(0, 9), random.randint(0, 9))
 
-class CBAeroSetting(Base):
+        return cls(name=name, version=version)
+
+
+class CBAeroSetting(Base, ToDictMixin):
     """
     """
     __tablename__ = "cbaero_settings"
@@ -98,8 +144,14 @@ class CBAeroSetting(Base):
     conf_file = Column(String(128))
     hash = Column(String(128), unique=True)
 
+    @classmethod
+    def generate_random(cls):
+        conf_file = get_first_name() + ".conf"
+        hash = str(random.randint(0, 100000))
+        return cls(conf_file=conf_file, hash=hash)
 
-class Cart3DSetting(Base):
+
+class Cart3DSetting(Base, ToDictMixin):
     """
     """
     __tablename__ = "cart3d_settings"
@@ -109,8 +161,14 @@ class Cart3DSetting(Base):
     cntl_file = Column(String(128))
     hash = Column(String(128), unique=True)
 
+    @classmethod
+    def generate_random(cls):
+        cntl_file = get_first_name() + ".cntl"
+        hash = str(random.randint(0, 100000))
+        return cls(cntl_file=cntl_file, hash=hash)
 
-class ToolSetting(Base):
+
+class ToolSetting(Base, ToDictMixin):
     """
     """
     __tablename__ = "tool_settings"
@@ -121,8 +179,18 @@ class ToolSetting(Base):
     cbaero_settings_fk: Mapped[int] = mapped_column(ForeignKey("cbaero_settings.id"))
     cart3d_settings_fk: Mapped[int] = mapped_column(ForeignKey("cart3d_settings.id"))
 
+    @classmethod
+    def generate_random(cls, num_settings):
+        tool_settings = ["cbaero_settings_fk", "cart3d_settings_fk"]
+        chosen_tool_settings_index = random.randint(0, len(tool_settings)-1)
 
-class ConfiguredTool(Base):
+        kwargs = {
+            tool_settings[chosen_tool_settings_index]: random.randint(0, num_settings-1)
+        }
+        return cls(**kwargs)
+
+
+class ConfiguredTool(Base, ToDictMixin):
     """
     """
     __tablename__ = "configured_tools"
@@ -133,8 +201,14 @@ class ConfiguredTool(Base):
     tool_fk: Mapped[int] = mapped_column(ForeignKey("tools.id"))
     tool_settings_fk: Mapped[int] = mapped_column(ForeignKey("tool_settings.id"))
 
+    @classmethod
+    def generate_random(cls, num_tools, num_tool_settings):
+        tool_fk = random.randint(0, num_tools-1)
+        tool_settings_fk = random.randint(0, num_tool_settings-1)
+        return cls(tool_fk=tool_fk, tool_settings_fk=tool_settings_fk)
 
-class ToolMeshAssociation(Base):
+
+class ToolMeshAssociation(Base, ToDictMixin):
     """
     """
     __tablename__ = "tool_mesh_associations"
@@ -146,8 +220,20 @@ class ToolMeshAssociation(Base):
     tool_fk: Mapped[int] = mapped_column(ForeignKey("tools.id"))
     configured_tool_fk: Mapped[int] = mapped_column(ForeignKey("configured_tools.id"))
 
+    @classmethod
+    def generate_random(cls, num_contributors: int, num_meshes: int, num_tools: int, num_configured_tools: int):
+        contributor_fk = random.randint(0, num_contributors-1)
+        mesh_fk = random.randint(0, num_meshes-1)
+        tool_fk = random.randint(0, num_tools-1)
+        configured_tool_fk = random.randint(0, num_configured_tools-1)
 
-class ToolGeometryAssociation(Base):
+        return cls(contributor_fk=contributor_fk, 
+                   mesh_fk=mesh_fk, 
+                   tool_fk=tool_fk, 
+                   configured_tool_fk=configured_tool_fk)
+
+
+class ToolGeometryAssociation(Base, ToDictMixin):
     """
     """
     __tablename__ = "tool_geometry_associations"
@@ -159,8 +245,19 @@ class ToolGeometryAssociation(Base):
     tool_fk: Mapped[int] = mapped_column(ForeignKey("tools.id"))
     configured_tool_fk: Mapped[int] = mapped_column(ForeignKey("configured_tools.id"))
 
+    @classmethod
+    def generate_random(cls, num_contributors: int, num_geometries: int, num_tools: int, num_configured_tools: int):
+        contributor_fk = random.randint(0, num_contributors-1)
+        geometry_fk = random.randint(0, num_geometries-1)
+        tool_fk = random.randint(0, num_tools-1)
+        configured_tool_fk = random.randint(0, num_configured_tools-1)
 
-class AeroResult(Base):
+        return cls(contributor_fk=contributor_fk, 
+                   geometry_fk=geometry_fk, 
+                   tool_fk=tool_fk, configured_tool_fk=configured_tool_fk)
+
+
+class AeroResult(Base, ToDictMixin):
     """
     """
     __tablename__ = "aero_results"
@@ -174,8 +271,22 @@ class AeroResult(Base):
     configured_tool_fk: Mapped[int] = mapped_column(ForeignKey("configured_tools.id"))
     mesh_fk: Mapped[int] = mapped_column(ForeignKey("meshes.id"))
 
+    @classmethod
+    def generate_random(cls, num_configured_tools: int, num_meshes: int):
+        mach = random.random()
+        alpha = random.random()
+        lift_coefficient = random.random()
+        drag_coefficient = random.random()
+        configured_tool_fk = random.randint(0, num_configured_tools-1)
+        mesh_fk = random.randint(0, num_meshes-1)
+        return cls(mach=mach,
+                   alpha=alpha,
+                   lift_coefficient=lift_coefficient,
+                   drag_coefficient=drag_coefficient,
+                   configured_tool_fk=configured_tool_fk,
+                   mesh_fk=mesh_fk)
 
-class Comment(Base):
+class Comment(Base, ToDictMixin):
     """
     """
     __tablename__ = "comments"
@@ -191,3 +302,29 @@ class Comment(Base):
     tool_mesh_association_fk = mapped_column(ForeignKey("tool_mesh_associations.id"))
     configured_tool_fk = mapped_column(ForeignKey("configured_tools.id"))
 
+    @classmethod
+    def generate_random(cls, 
+                        num_contributors: int,
+                        num_systems: int,
+                        num_geometries: int,
+                        num_meshes: int,
+                        num_tool_mesh_associations: int,
+                        num_configured_tools: int
+                        ):
+
+        title = lorem.sentence()
+        body = lorem.paragraph()
+        contributor_fk = random.randint(0, num_contributors-1)
+
+        foreign_keys = [
+            ("system_fk", random.randint(0, num_systems-1)),
+            ("geometry_fk", random.randint(0, num_geometries-1)),
+            ("mesh_fk", random.randint(0, num_meshes)),
+            ("tool_mesh_association_fk", random.randint(0, num_tool_mesh_associations-1)),
+            ("configured_tool_fk", random.randint(0, num_configured_tools-1))
+        ]
+        foreign_key_index = random.randint(0, len(foreign_keys)-1)
+        foreign_key = foreign_keys[foreign_key_index]
+        kwargs = {foreign_key[0]: foreign_key[1]}
+
+        return cls(title=title, body=body, contributor_fk=contributor_fk, **kwargs)
